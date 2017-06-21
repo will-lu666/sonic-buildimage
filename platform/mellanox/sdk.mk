7f1097e84b315a521ad06c71143883157c40062a
Qi Luo <qiluo-msft@users.noreply.github.com>
2017-06-21 02:07:22 +0000

    Update Mellanox firmware, SDK, SAI 1.0.2

--- platform/mellanox/sdk.mk
+++ platform/mellanox/sdk.mk
@@ -1,5 +1,5 @@
-MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/f3a5883f93d7ef6ad8aa002e4e5abaa22fb7adfd/sdk
-MLNX_SDK_VERSION = 4.2.4000
+MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/sonicv1.0.2/sdk
+MLNX_SDK_VERSION = 4.2.5008
 MLNX_SDK_RDEBS += $(APPLIBS) $(IPROUTE2) $(SX_ACL_RM) $(SX_COMPLIB) \
 		  $(SX_EXAMPLES) $(SX_GEN_UTILS) $(SX_SCEW) $(SX_SDN_HAL) \
 		  $(SXD_LIBS) $(TESTX)
