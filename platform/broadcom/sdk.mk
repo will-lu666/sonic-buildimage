8fe59b59e513afcf1733ef269f571f3c623beaeb
Qi Luo <qiluo-msft@users.noreply.github.com>
2017-06-22 00:55:08 +0000

    Update libsaibcm version

--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,8 +1,8 @@
-BRCM_OPENNSL = libopennsl_3.2.3.1+0~20170605184634.7~1.gbp4a0717_amd64.deb
+BRCM_OPENNSL = libopennsl_3.2.3.1_amd64.deb
 # TODO: upload new SDK build to blob
 $(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.3.1+0~20170605184634.7~1.gbp4a0717_amd64.deb"
 
-BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.3.1+0~20170605184634.7~1.gbp4a0717_amd64.deb
+BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.3.1_amd64.deb
 # TODO: upload new SDK build to blob
 $(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.3.1+0~20170605184634.7~1.gbp4a0717_amd64.deb"
 
