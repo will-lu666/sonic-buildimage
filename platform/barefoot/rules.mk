547049a806f5fba25477b79ee41bbca6205f2b25
kram <kram@barefootnetworks.com>
2018-01-02 18:29:37 -0800

    Allow multi platform support - infra (more changes to follow)

--- platform/barefoot/rules.mk
+++ platform/barefoot/rules.mk
@@ -8,12 +8,13 @@ include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/libsaithrift-dev.mk
 include $(PLATFORM_PATH)/python-saithrift.mk
 include $(PLATFORM_PATH)/docker-ptf-bfn.mk
+include $(PLATFORM_PATH)/bfn-platform.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM)
 
 # Inject sai into sairedis
-$(LIBSAIREDIS)_DEPENDS += $(BFN_SAI) #$(LIBSAITHRIFT_DEV_BFN)
+$(LIBSAIREDIS)_DEPENDS += $(BFN_SAI)  $(BFN_PLATFORM)#$(LIBSAITHRIFT_DEV_BFN)
 
 # Runtime dependency on sai is set only for syncd
-$(SYNCD)_RDEPENDS += $(BFN_SAI)
+$(SYNCD)_RDEPENDS += $(BFN_SAI) $(BFN_PLATFORM)
