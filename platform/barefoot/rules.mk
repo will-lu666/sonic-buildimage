786a85dcfb9ccade69a66ecbe0a5278f5910dc47
Brand <brand.huang@wnc.com.tw>
2018-02-01 11:17:01 +0800

    Modify for Barefoot suggest

--- platform/barefoot/rules.mk
+++ platform/barefoot/rules.mk
@@ -10,12 +10,13 @@ include $(PLATFORM_PATH)/libsaithrift-dev.mk
 include $(PLATFORM_PATH)/python-saithrift.mk
 include $(PLATFORM_PATH)/docker-ptf-bfn.mk
 include $(PLATFORM_PATH)/bfn-platform.mk
+include $(PLATFORM_PATH)/bfn-platform-wnc.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM)
 
 # Inject sai into sairedis
-$(LIBSAIREDIS)_DEPENDS += $(BFN_SAI)  $(BFN_PLATFORM)#$(LIBSAITHRIFT_DEV_BFN)
+$(LIBSAIREDIS)_DEPENDS += $(BFN_SAI) $(BFN_PLATFORM) $(BFN_WNC_PLATFORM)#$(LIBSAITHRIFT_DEV_BFN)
 
 # Runtime dependency on sai is set only for syncd
-$(SYNCD)_RDEPENDS += $(BFN_SAI) $(BFN_PLATFORM)
+$(SYNCD)_RDEPENDS += $(BFN_SAI) $(BFN_PLATFORM) $(BFN_WNC_PLATFORM)
