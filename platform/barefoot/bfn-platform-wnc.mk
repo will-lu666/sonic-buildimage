5bc6dbca5031c24dc5c06fdc96a092175ef1fc59
YaoTien <hyaotien@gmail.com>
2018-02-03 10:54:21 +0800

    Update bfn-platform-wnc.mk
    Update parameter name

--- platform/barefoot/bfn-platform-wnc.mk
+++ platform/barefoot/bfn-platform-wnc.mk
@@ -1,5 +1,5 @@
-BFN_PLATFORM = bfnplatformwnc_1.0.0_amd64.deb
-$(BFN_PLATFORM)_URL = "https://github.com/YaoTien/download/raw/master/sonic/sde/7_0_0_18/bfnplatformwnc_1.0.0_amd64.deb"
+BFN_WNC_PLATFORM = bfnplatformwnc_1.0.0_amd64.deb
+$(BFN_WNC_PLATFORM)_URL = "https://github.com/YaoTien/download/raw/master/sonic/sde/7_0_0_18/bfnplatformwnc_1.0.0_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_WNC_PLATFORM) # $(BFN_SAI_DEV)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_WNC_PLATFORM)
