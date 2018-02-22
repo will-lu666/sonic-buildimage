4f4422663e01a70b0bc64e7a0d5ab950217b2875
wadelnn <wadelnn@users.noreply.github.com>
2018-02-22 20:15:12 +0800

    Add Ingrasys S9180 platform
    Signed-off-by: Wade He <chihen.he@gmail.com>

--- platform/barefoot/rules.mk
+++ platform/barefoot/rules.mk
@@ -1,6 +1,7 @@
 include $(PLATFORM_PATH)/platform-modules-bfn.mk
 include $(PLATFORM_PATH)/platform-modules-bfn-montara.mk
 include $(PLATFORM_PATH)/platform-modules-wnc-osw1800.mk
+include $(PLATFORM_PATH)/platform-modules-ingrasys.mk
 include $(PLATFORM_PATH)/bfn-sai.mk
 include $(PLATFORM_PATH)/docker-syncd-bfn.mk
 include $(PLATFORM_PATH)/docker-syncd-bfn-rpc.mk
@@ -11,12 +12,12 @@ include $(PLATFORM_PATH)/python-saithrift.mk
 include $(PLATFORM_PATH)/docker-ptf-bfn.mk
 include $(PLATFORM_PATH)/bfn-platform.mk
 include $(PLATFORM_PATH)/bfn-platform-wnc.mk
-
+include $(PLATFORM_PATH)/bfn-platform-ingrasys.mk
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM)
 
 # Inject sai into sairedis
-$(LIBSAIREDIS)_DEPENDS += $(BFN_SAI) $(BFN_WNC_PLATFORM) $(BFN_PLATFORM)#$(LIBSAITHRIFT_DEV_BFN)
+$(LIBSAIREDIS)_DEPENDS += $(BFN_SAI) $(BFN_WNC_PLATFORM) $(BFN_INGRASYS_PLATFORM) $(BFN_PLATFORM)#$(LIBSAITHRIFT_DEV_BFN)
 
 # Runtime dependency on sai is set only for syncd
-$(SYNCD)_RDEPENDS += $(BFN_SAI) $(BFN_WNC_PLATFORM) $(BFN_PLATFORM)
+$(SYNCD)_RDEPENDS += $(BFN_SAI) $(BFN_WNC_PLATFORM) $(BFN_INGRASYS_PLATFORM) $(BFN_PLATFORM)
