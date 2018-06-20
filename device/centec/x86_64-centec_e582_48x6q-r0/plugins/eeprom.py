c9899b6eae0d9ddd0528b7e335aa65d4313092bb
mkbalani <mk_sagarbalani@yahoo.com>
2018-06-20 15:31:30 -0700

    Revert "Address upstream comments (#11)"
    This reverts commit 559132103e5c73e43f4282d1559ede03f16abfea.

--- device/centec/x86_64-centec_e582_48x6q-r0/plugins/eeprom.py
+++ device/centec/x86_64-centec_e582_48x6q-r0/plugins/eeprom.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+#############################################################################
+# Centec E582-48X6Q
+#
+# Platform and model specific eeprom subclass, inherits from the base class,
+# and provides the followings:
+# - the eeprom format definition
+# - specific encoder/decoder if there is special need
+#############################################################################
+
 try:
     import exceptions
     import binascii
@@ -8,17 +17,15 @@ try:
     import warnings
     import os
     import sys
+    import subprocess
     from sonic_eeprom import eeprom_base
     from sonic_eeprom import eeprom_tlvinfo
-    import subprocess
 except ImportError, e:
     raise ImportError (str(e) + "- required module not found")
 
+
 class board(eeprom_tlvinfo.TlvInfoDecoder):
-    _TLV_INFO_MAX_LEN = 256
+
     def __init__(self, name, path, cpld_root, ro):
-        self.eeprom_path = "/sys/bus/i2c/devices/1-0057/eeprom"
-        #Two i2c buses might get flipped order, check them both.
-        if not os.path.exists(self.eeprom_path):
-            self.eeprom_path = "/sys/bus/i2c/devices/0-0057/eeprom"
+        self.eeprom_path = "/sys/class/i2c-adapter/i2c-0/0-0057/eeprom"
         super(board, self).__init__(self.eeprom_path, 0, '', True)
